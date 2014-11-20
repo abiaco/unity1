0e2d838894fbb3b28ba3b8d3fd92f95b28904410
Andrei Iacob <iacob.andrei986@gmail.com>
2014-11-20 16:14:50 +0000

    Commit 8: GAME

---
+++ Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyControl : MonoBehaviour {
+    public float speed;
+    public int power;
+    public int health;
+    public GUIText healthText;
+	// Use this for initialization
+	void Start () {
+        power = 10;
+        health = 100;
+        setHealthText();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveX = Random.value;
+        float moveY = Random.value;
+        Vector3 move = new Vector3(moveX, 0.0f, moveY);
+        rigidbody.AddForce(move * speed * Time.deltaTime);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerControl player = (PlayerControl) other.GetComponent("PlayerControl");
+            health -= player.power;
+            setHealthText();
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerControl player = (PlayerControl)other.collider.GetComponent("PlayerControl");
+            player.health -= power;
+            setHealthText();
+            if (player.health < 1)
+            {
+                player.healthText.text = " ";
+                player.gameObject.SetActive(false);
+                
+            }
+        }
+    }
+    void setHealthText()
+    {
+        healthText.text = "Enemy Health: " + health.ToString();
+    }
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,14 +3,65 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
     public float speed;
+    private int count;
+    public int health;
+    public GUIText countText;
+    public int power;
+    public GUIText healthText;
 
+    void Start()
+    {
+        count = 0;
+        power = 5;
+        health = 100;
+        setCountText();
+        setHealthText();
+    }
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rigidbody.AddForce(movement * speed * Time.deltaTime);
-    
+        
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+            power += 2;
+        }
+
+        
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            EnemyControl enemy = (EnemyControl)other.collider.GetComponent("EnemyControl");
+            enemy.health -= power;
+            setHealthText();
+            if (enemy.health < 1)
+            {
+                enemy.healthText.text = " ";
+                enemy.gameObject.SetActive(false);
+                
+            }
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+    void setHealthText()
+    {
+        healthText.text = "Player Health: " + health.ToString();
+    }
 }
