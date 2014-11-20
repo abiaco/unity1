7da8d2ba076b46f03c3c9fff311b234349ba84ea
Andrei Iacob <iacob.andrei986@gmail.com>
2014-11-20 12:30:24 +0000

    fifth commit: pickup

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -10,6 +10,7 @@ public class PlayerControl : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rigidbody.AddForce(movement * speed * Time.deltaTime);
+    
     }
 
 }
